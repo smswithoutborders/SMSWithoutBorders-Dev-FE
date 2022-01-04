50953f2613aa25352d3f8b8fc44901735e0cf271
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-01-04 15:26:00 +0100

    update: add Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,56 +1,41 @@
-import React, { Fragment } from "react";
+import React from "react";
 import logo from "images/logo.png";
-import { Menu, Transition } from "@headlessui/react";
-import { BsChevronDown, BsPersonCircle } from "react-icons/bs";
-import { FiLogOut } from "react-icons/fi";
+import { FiLogOut, FiMenu } from "react-icons/fi";
+import PropTypes from "prop-types";
 
-const Navbar = () => {
+const Navbar = ({ open, onToggle }) => {
   const handleLogOut = () => {
     // handle logout here
   };
 
   return (
-    <nav>
-      <div className="flex flex-row flex-wrap items-center justify-between p-2">
+    <nav className="px-4 py-3 text-white bg-gray-800">
+      <div className="flex flex-row flex-wrap items-center justify-between">
         <div className="flex items-center">
-          <img src={logo} alt="logo" className="inline-block w-8 h-8 mx-auto" />
-          <span className="ml-2 text-xl">SMSWithoutBorders</span>
+          <FiMenu
+            size={20}
+            className="mr-4 cursor-pointer"
+            onClick={() => onToggle()}
+          />
+          <img src={logo} alt="logo" className="inline-block w-6 h-6 mx-auto" />
+          <span className="ml-2 font-medium">SMSWithoutBorders</span>
+          <span className="ml-1 tracking-wide text-light">Developer</span>
         </div>
-
-        <Menu as="div" className="relative inline-block">
-          <div>
-            <Menu.Button className="inline-flex items-center align-center">
-              <BsPersonCircle size={24} />
-              <BsChevronDown className="ml-2" />
-            </Menu.Button>
-          </div>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute right-0 w-56 mt-4 origin-top-right bg-white divide-y divide-gray-100 rounded-md focus:outline-none">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    onClick={() => handleLogOut()}
-                    className="flex items-center w-full p-2 rounded"
-                  >
-                    <FiLogOut className="mr-2" />
-                    <span>Logout</span>
-                  </button>
-                )}
-              </Menu.Item>
-            </Menu.Items>
-          </Transition>
-        </Menu>
+        <button
+          onClick={() => handleLogOut()}
+          className="items-center hidden px-2 py-1 text-sm border rounded-lg md:flex"
+        >
+          <FiLogOut className="mr-1" />
+          <span>Logout</span>
+        </button>
       </div>
     </nav>
   );
+};
+
+Navbar.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onToggle: PropTypes.func.isRequired,
 };
 
 export default Navbar;
