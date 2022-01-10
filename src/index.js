d1c88049ed03840c538af6259639dd81a5e35d1d
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-01-10 10:51:29 +0100

    update: add redux store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
+import { store } from "./features";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
