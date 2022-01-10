526349c627442faefc2e29c4046fba3938cfe203
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2022-01-10 14:05:16 +0100

    feat: integrate key generation

--- src/pages/Credentials.js
+++ src/pages/Credentials.js
@@ -1,28 +1,69 @@
 import React, { useState, Fragment } from "react";
 import { TabBar, Loader, ClipBoard, ToggleButton } from "components";
+import { useSelector, useDispatch } from "react-redux";
+import { authSelector, credentialsSelector, saveCredentials } from "features";
+import { useNewCredentialsMutation } from "services/api";
+import toast from "react-hot-toast";
 
 const Credentials = () => {
-  const [data, setData] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [showUID, setShowUID] = useState(false);
+  const [showAuthID, setShowAuthID] = useState(false);
+  const [showAuthKey, setShowAuthKey] = useState(false);
+  const credentials = useSelector(credentialsSelector);
+  const auth = useSelector(authSelector);
+  const dispatch = useDispatch();
+  const [newCredentials, { isLoading }] = useNewCredentialsMutation();
 
-  function toggleUID() {
-    setShowUID(!showUID);
+  // toggle key visibility
+  function toggleAuthID() {
+    setShowAuthID(!showAuthID);
+  }
+  function toggleAuthKey() {
+    setShowAuthKey(!showAuthKey);
   }
 
-  function handleCredGeneration() {
-    setLoading(true);
-    setTimeout(() => {
-      setData(!data);
-      setLoading(false);
-    }, 1000);
+  // generate credentials
+  async function handleCredGeneration() {
+    try {
+      const newCreds = await newCredentials(auth).unwrap();
+      dispatch(saveCredentials(newCreds));
+    } catch (error) {
+      switch (error.status) {
+        case 400:
+          toast.error("An error occured. Please contact support");
+          break;
+        case 401:
+          toast.error(
+            "Sorry you are not authorized to use this service. Please contact support"
+          );
+          break;
+        case 409:
+          toast.error(
+            "There is a possible duplicate of this account please contact support"
+          );
+          break;
+
+        case 429:
+          toast.error(
+            "Too many failed attempts please wait a while and try again"
+          );
+          break;
+        case 500:
+          toast.error("A critical error occured. Please contact support");
+          break;
+        case "FETCH_ERROR":
+          toast.error("An error occured, please check your network try again");
+          break;
+        default:
+          toast.error("An error occured, please try again");
+      }
+    }
   }
 
-  if (loading) return <Loader message="Processing please wait ..." />;
+  if (isLoading) return <Loader message="Processing please wait ..." />;
   return (
     <div className="flex-1 w-full">
       <TabBar title="Credentials">
-        {!data ? (
+        {!credentials.authKey ? (
           <button
             className="px-6 py-1 text-white bg-indigo-500 rounded-md"
             onClick={() => handleCredGeneration()}
@@ -44,29 +85,66 @@ const Credentials = () => {
           Use this key in your application by passing it with key=API_KEY
           parameter.
         </p>
-        {data ? (
-          <div className="my-2">
-            <h4>User ID</h4>
-            <div className="flex flex-col items-start md:flex-row">
-              <div className="mr-3">
-                <p className="my-0 break-words">
-                  xxxxxxx xxxxxxx xxxxxxx xxxxxxx xxxxxxx xxxxxxx xxxxxxx
-                </p>
-                <small className="text-gray-500 break-words">
-                  Use this key in your application by passing it with
-                  user=UserID parameter.
-                </small>
-              </div>
-              <div className="flex my-2 md:my-0 md:p-1.5">
-                <ToggleButton
-                  className="mr-3"
-                  value={showUID}
-                  toggleFunc={toggleUID}
-                />
-                <ClipBoard size={16} text="UserID" value="XXXXXXXXXsome" />
+        {credentials.authKey ? (
+          <Fragment>
+            <div className="my-2">
+              <h4>Auth ID</h4>
+              <div className="flex flex-col items-start md:flex-row">
+                <div className="mr-3">
+                  <p className="my-0 break-words">
+                    {showAuthID
+                      ? credentials.authID
+                      : "xxxxxxx xxxxxxx xxxxxxx xxxxxxx xxxxxxx xxxxxxx xxxxx"}
+                  </p>
+                  <small className="text-gray-500 break-words">
+                    Use this key in your application by passing it with
+                    auth_id="Auth ID" parameter.
+                  </small>
+                </div>
+                <div className="flex my-2 md:my-0 md:p-1.5">
+                  <ToggleButton
+                    className="mr-3"
+                    value={showAuthID}
+                    toggleFunc={toggleAuthID}
+                  />
+                  <ClipBoard
+                    size={16}
+                    text="Auth ID"
+                    value={credentials.authID}
+                  />
+                </div>
               </div>
             </div>
-          </div>
+
+            <div className="my-2">
+              <h4>Auth Key</h4>
+              <div className="flex flex-col items-start md:flex-row">
+                <div className="mr-3">
+                  <p className="my-0 break-words">
+                    {showAuthKey
+                      ? credentials.authKey
+                      : "xxxxxxx xxxxxxx xxxxxxx xxxxxxx xxxxxxx xxxxxxx xxxxx"}
+                  </p>
+                  <small className="text-gray-500 break-words">
+                    Use this key in your application by passing it with
+                    auth_key="Auth Key" parameter.
+                  </small>
+                </div>
+                <div className="flex my-2 md:my-0 md:p-1.5">
+                  <ToggleButton
+                    className="mr-3"
+                    value={showAuthKey}
+                    toggleFunc={toggleAuthKey}
+                  />
+                  <ClipBoard
+                    size={16}
+                    text="Auth Key"
+                    value={credentials.authKey}
+                  />
+                </div>
+              </div>
+            </div>
+          </Fragment>
         ) : (
           <Fragment>
             <h3>Generate Keys</h3>
